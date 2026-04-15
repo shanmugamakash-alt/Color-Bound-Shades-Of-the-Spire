d1578ecc6cb8459009bafcda2423985414e565f7
Shanmugamakash Rajaravisankar <304534@friscoisd.org>
2026-04-15 14:30:06 -0500

    alomst

---
+++ Color_Bound_Shades_Of_the_Spire/BlueDoor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Color_Bound_Shades_Of_the_Spire
+{
+    class Bluedoor
+    {
+        Tile[,] tiles;
+        public Bluedoor()
+        {
+            
+        }
+
+
+        public void nextRoom(Player player, Level level)
+        {
+            if(player.rec.Intersects(tiles[0, 1].GetRec()))
+            {
+
+            }
+        }
+
+        public void tile(Tile[,] t)
+        {
+            tiles = t;
+        }
+    }
+}
