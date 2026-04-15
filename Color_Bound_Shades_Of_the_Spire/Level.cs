21b53ac359089044ec769e87156be49a4e33b5dd
Devdarsh Nambiar <253255@friscoisd.org>
2026-04-14 23:02:02 -0500

    finsihed with 2 levels

--- Color_Bound_Shades_Of_the_Spire/Level.cs
+++ Color_Bound_Shades_Of_the_Spire/Level.cs
@@ -16,7 +16,7 @@ namespace Color_Bound_Shades_Of_the_Spire
     {
         string[] fileNames;
         Tile[,] tiles;
-        Texture2D[] Textures;
+        public Texture2D[] Textures;
         public int room;
         public int tileSize;
         public int offset, velocityX;
@@ -24,6 +24,9 @@ namespace Color_Bound_Shades_Of_the_Spire
         public int checkpoint;
         public float scale;
         public bool levelComplete;
+        public List<YellowGiver> YGList;
+        public List<YellowReciever> YRList;
+        public List<YellowDoor> YDList;
         public Level(string[] fileNames, Texture2D[] textures)
         {
             this.fileNames = fileNames;
@@ -36,20 +39,40 @@ namespace Color_Bound_Shades_Of_the_Spire
             Textures = textures;
             initial = true;
             checkpoint = 0;
+            YGList = new List<YellowGiver>();
+            YRList = new List<YellowReciever>();
+            YDList = new List<YellowDoor>();
             LoadTiles(this.fileNames);
         }
 
-        //add player as a paramter to check if its reached the middle of the screen, then move to the left (change what you have cuase its useless)
         public void Update(Player player, KeyboardState kb, LevelLoader LL)
         {
-            player.collision(tiles, this, LL);
-            if (room != player.room)
-            {
-                LoadTiles(fileNames);
-                room = player.room;
-            }
             player.move(kb, this);
             player.UpdateRectangle();
+            player.collision(tiles, this, LL);
+            for (int i = 0; i < YGList.Count; i++)
+            {
+                YGList[i].colision(player);
+            }
+            for (int i = 0; i < YRList.Count; i++)
+            {
+                for (int j = 0; j < YGList.Count; j++)
+                {
+                    if (YGList[j].touched && YRList[i].activatedBy == null)
+                        YRList[i].colision(player, Textures, YGList[j]);
+                }
+            }
+            for (int i = 0; i < YDList.Count; i++)
+            {
+                YDList[i].colision(player, this);
+            }
+            if (initial)
+            {
+                YGList.Clear();
+                YRList.Clear();
+                YDList.Clear();
+                LoadTiles(fileNames);
+            }
             if (levelComplete)
             {
                 LL.CurrentLevel = (LevelLoader.currentLevel)5;
@@ -57,6 +80,8 @@ namespace Color_Bound_Shades_Of_the_Spire
         }
         public void LoadTiles(string[] fileNames)
         {
+            Console.WriteLine("ROOM: " + room);
+            Console.WriteLine("FILES LENGTH: " + fileNames.Length);
             int x = 0;
             int y = 0;
             string path = fileNames[room];
@@ -99,10 +124,16 @@ namespace Color_Bound_Shades_Of_the_Spire
                 case "f":
                     tiles[x,y] = new Tile(Textures[0], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.floor);
                     break;
+                case "Yf":
+                    tiles[x, y] = new Tile(Textures[6], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.floor);
+                    break;
                 case "0":
                     tiles[x, y] = new Tile(Textures[1], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.air);
                     break;
-                case "s":
+                case "Y0":
+                    tiles[x, y] = new Tile(Textures[7], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.air);
+                    break;
+                case "sU":
                     tiles[x, y] = new Tile(Textures[2], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.spike);
                     break;
                 case "E":
@@ -110,6 +141,9 @@ namespace Color_Bound_Shades_Of_the_Spire
                     break;
                 case "S":
                     tiles[x, y] = new Tile(Textures[1], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.start);
+                    break;
+                case "YS":
+                    tiles[x, y] = new Tile(Textures[7], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.start);
                     break;
                 case "c":
                     tiles[x, y] = new Tile(Textures[3], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.checkpoint);
@@ -120,20 +154,41 @@ namespace Color_Bound_Shades_Of_the_Spire
                 case "L":
                     tiles[x, y] = new Tile(Textures[4], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.LevelHub);
                     break;
-                case "R":
+                case "RE":
                     tiles[x, y] = new Tile(Textures[4], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.RedEntrance);
                     break;
-                case "B":
+                case "BE":
                     tiles[x, y] = new Tile(Textures[4], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.BlueEntrance);
                     break;
-                case "Y":
-                    tiles[x, y] = new Tile(Textures[4], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.YellowEntrance);
+                case "YEU":
+                    tiles[x, y] = new Tile(Textures[5], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.YellowEntrance);
+                    break;
+                case "YED":
+                    tiles[x, y] = new Tile(Textures[6], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize), Tile.TileType.YellowEntrance);
+                    break;
+                case "YDUL":
+                    YDList.Add(new YellowDoor(Textures[8], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
+                    break;
+                case "YDUR":
+                    YDList.Add(new YellowDoor(Textures[9], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
+                    break;
+                case "YDDL":
+                    YDList.Add(new YellowDoor(Textures[10], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
+                    break;
+                case "YDDR":
+                    YDList.Add(new YellowDoor(Textures[11], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
+                    break;
+                case "YR":
+                    YRList.Add(new YellowReciever(Textures[13], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
+                    break;
+                case "YG":
+                    YGList.Add(new YellowGiver(Textures[12], new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)));
                     break;
 
 
             }
         }
-        public void DrawAll(SpriteBatch spriteBatch)
+        public void DrawAll(SpriteBatch spriteBatch, Player player)
         {
             if (tiles == null) return;
             for (int i = 0; i < tiles.GetLength(0); i++)
@@ -153,6 +208,18 @@ namespace Color_Bound_Shades_Of_the_Spire
                             spriteBatch.Draw(Textures[1], drawRect, Color.White);
                     }
                 }
+            }
+            for (int i = 0; i < YGList.Count; i++)
+            {
+                YGList[i].Draw(spriteBatch, player, this);
+            }
+            for (int i = 0; i < YRList.Count; i++)
+            {
+                YRList[i].Draw(spriteBatch);
+            }
+            for (int i = 0; i < YDList.Count; i++)
+            {
+                YDList[i].Draw(spriteBatch);
             }
         }
     }
---
+++ Color_Bound_Shades_Of_the_Spire/YellowDoor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Color_Bound_Shades_Of_the_Spire
+{
+    public class YellowDoor
+    {
+        Texture2D T;
+        Rectangle R;
+        public bool isOpen;
+        public YellowDoor(Texture2D t, Rectangle r)
+        {
+            T = t;
+            R = r;
+            isOpen = false;
+        }
+
+        public void colision(Player player, Level level)
+        {
+            bool allOn = true; 
+            for (int i = 0; i < level.YRList.Count; i++)
+            {
+                if (!level.YRList[i].isOn)
+                {
+                    allOn = false;
+                    break;
+                }
+            }
+            if (allOn && player.rec.Intersects(R))
+            {
+                level.initial = true;
+                level.room += 1;
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(T, R, Color.White);
+        }
+    }
+}
