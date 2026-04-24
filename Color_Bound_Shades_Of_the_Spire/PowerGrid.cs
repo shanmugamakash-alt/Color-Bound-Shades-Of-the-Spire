a4b6b5cdca91f08b5cbcbf37171db44e33e67061
Devdarsh Nambiar <253255@friscoisd.org>
2026-04-24 14:28:03 -0500

    started laser varients

---
+++ Color_Bound_Shades_Of_the_Spire/PowerGrid.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Color_Bound_Shades_Of_the_Spire
+{
+    public class PowerGrid
+    {
+        Texture2D T;
+        Rectangle R;
+        public int numReciever;
+
+        public PowerGrid(Texture2D t, Rectangle r, Level level)
+        {
+            T = t;
+            R = r;
+            numReciever = level.YRList.Count;
+        }
+
+        public void colision(Player player, Level level)
+        {
+            int numOn = 0;
+            if (player.rec.Intersects(R))
+            {
+                for (int i = 0; i < level.YRList.Count; i++)
+                {
+                    if (level.YRList[i].isOn)
+                    {
+                        numOn++;
+                    }
+                }
+                if (numOn == numReciever)
+                {
+                    for (int i = 0; i < level.YLVVList.Count; i++)
+                    {
+                        level.YLVVList[i].isOn = false;
+                    }
+                }
+            }
+        }
+    }
+}
--- Color_Bound_Shades_Of_the_Spire/YLaserVertVarient.cs
+++ Color_Bound_Shades_Of_the_Spire/YLaserVertVarient.cs
@@ -13,5 +13,26 @@ namespace Color_Bound_Shades_Of_the_Spire
 {
     public class YLaserVertVarient
     {
+        public Texture2D T;
+        public Rectangle R;
+        public bool isOn;
+        public YLaserVertVarient(Texture2D t, Rectangle r)
+        {
+            T = t;
+            R = r;
+            isOn = true;
+        }
+
+        public void colision(Player player, Texture2D[] texs)
+        {
+            if (player.rec.Intersects(R) && isOn)
+            {
+                player.dead = true;
+            }
+            if (isOn == false)
+            {
+                T = texs[7];
+            }
+        }
     }
 }
